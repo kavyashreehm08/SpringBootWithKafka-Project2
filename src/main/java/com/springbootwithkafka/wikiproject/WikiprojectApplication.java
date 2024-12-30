17ef7a529de63968dff06e40273bbfff57ef735f
Kavyashree HM <123004602+kavyashreehm08@users.noreply.github.com>
2024-12-29 23:20:09 -0700

    first commit

---
+++ src/main/java/com/springbootwithkafka/wikiproject/WikiprojectApplication.java
@@ -0,0 +1,13 @@
+package com.springbootwithkafka.wikiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikiprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikiprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootwithkafka/wikiproject/WikiprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootwithkafka.wikiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
